27e7b90751485371a1f2f28630293f6d540863c3
Jonas <jonasdasilvajesus@outlook.com>
2023-02-03 10:42:30 -0300

    fix: removing test code

--- src/main/java/com/rpg/RpgApplication.java
+++ src/main/java/com/rpg/RpgApplication.java
@@ -6,15 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class RpgApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RpgApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World!";
 	}
 }
