6eab48c16861463399a00251c421776105730647
Jonas <jonasdasilvajesus@outlook.com>
2023-02-13 12:47:55 -0300

    feat: creating the players dto

--- src/main/java/com/rpg/dto/PlayersDto.java
+++ src/main/java/com/rpg/dto/PlayersDto.java
@@ -1,2 +1,226 @@
-package com.rpg.dto;public class PlayersDto {
+package com.rpg.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class PlayersDto {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String playerName;
+
+    @NotBlank
+    private int level;
+
+    @NotBlank
+    private int exp;
+
+    @NotBlank
+    private String background;
+
+    @NotBlank
+    private String alignment;
+
+    @NotBlank
+    private int proficiencyBonus;
+
+    @NotBlank
+    private int inspiration;
+
+    @NotBlank
+    private int armorClass;
+
+    @NotBlank
+    private int initiative;
+
+    @NotBlank
+    private int passiveWisdom;
+
+    @NotBlank
+    private int speed;
+
+    @NotBlank
+    private int currentHitPoints;
+
+    @NotBlank
+    private int hitDice;
+
+    @NotBlank
+    private String personalityTraits;
+
+    @NotBlank
+    private String ideals;
+
+    @NotBlank
+    private String bonds;
+
+    @NotBlank
+    private String flaws;
+
+    @NotBlank
+    private String featuresAndTraits;
+
+    @NotBlank
+    private String otherProficienciesAndLanguages;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public void setBackground(String background) {
+        this.background = background;
+    }
+
+    public String getAlignment() {
+        return alignment;
+    }
+
+    public void setAlignment(String alignment) {
+        this.alignment = alignment;
+    }
+
+    public int getProficiencyBonus() {
+        return proficiencyBonus;
+    }
+
+    public void setProficiencyBonus(int proficiencyBonus) {
+        this.proficiencyBonus = proficiencyBonus;
+    }
+
+    public int getInspiration() {
+        return inspiration;
+    }
+
+    public void setInspiration(int inspiration) {
+        this.inspiration = inspiration;
+    }
+
+    public int getArmorClass() {
+        return armorClass;
+    }
+
+    public void setArmorClass(int armorClass) {
+        this.armorClass = armorClass;
+    }
+
+    public int getInitiative() {
+        return initiative;
+    }
+
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
+
+    public int getPassiveWisdom() {
+        return passiveWisdom;
+    }
+
+    public void setPassiveWisdom(int passiveWisdom) {
+        this.passiveWisdom = passiveWisdom;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getCurrentHitPoints() {
+        return currentHitPoints;
+    }
+
+    public void setCurrentHitPoints(int currentHitPoints) {
+        this.currentHitPoints = currentHitPoints;
+    }
+
+    public int getHitDice() {
+        return hitDice;
+    }
+
+    public void setHitDice(int hitDice) {
+        this.hitDice = hitDice;
+    }
+
+    public String getPersonalityTraits() {
+        return personalityTraits;
+    }
+
+    public void setPersonalityTraits(String personalityTraits) {
+        this.personalityTraits = personalityTraits;
+    }
+
+    public String getIdeals() {
+        return ideals;
+    }
+
+    public void setIdeals(String ideals) {
+        this.ideals = ideals;
+    }
+
+    public String getBonds() {
+        return bonds;
+    }
+
+    public void setBonds(String bonds) {
+        this.bonds = bonds;
+    }
+
+    public String getFlaws() {
+        return flaws;
+    }
+
+    public void setFlaws(String flaws) {
+        this.flaws = flaws;
+    }
+
+    public String getFeaturesAndTraits() {
+        return featuresAndTraits;
+    }
+
+    public void setFeaturesAndTraits(String featuresAndTraits) {
+        this.featuresAndTraits = featuresAndTraits;
+    }
+
+    public String getOtherProficienciesAndLanguages() {
+        return otherProficienciesAndLanguages;
+    }
+
+    public void setOtherProficienciesAndLanguages(String otherProficienciesAndLanguages) {
+        this.otherProficienciesAndLanguages = otherProficienciesAndLanguages;
+    }
 }
