417e3e7ab5dde9a312b70128421b1c374d44ff6b
Jonas <jonasdasilvajesus@outlook.com>
2023-02-13 12:49:54 -0300

    feat: creating the players service

--- src/main/java/com/rpg/service/PlayersService.java
+++ src/main/java/com/rpg/service/PlayersService.java
@@ -1,2 +1,20 @@
-package com.rpg.service;public class PlayersService {
+package com.rpg.service;
+
+import com.rpg.model.PlayersModel;
+import com.rpg.repository.PlayersRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PlayersService {
+    final PlayersRepository playersRepository;
+
+    public PlayersService(PlayersRepository playersRepository) {
+        this.playersRepository = playersRepository;
+    }
+
+    @Transactional
+    public PlayersModel save(PlayersModel playersModel) {
+        return playersRepository.save(playersModel);
+    }
 }
