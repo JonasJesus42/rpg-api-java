e648ac80295b36a9e88665c3a8413ec951705bad
Jonas <jonasdasilvajesus@outlook.com>
2023-02-13 12:49:20 -0300

    feat: creating the players repository

--- src/main/java/com/rpg/repository/PlayersRepository.java
+++ src/main/java/com/rpg/repository/PlayersRepository.java
@@ -1,2 +1,13 @@
-package com.rpg.repository;public interface PlayersRepository {
+package com.rpg.repository;
+
+import com.rpg.model.PlayersModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PlayersRepository extends JpaRepository<PlayersModel, UUID>, JpaSpecificationExecutor<PlayersModel> {
+
 }
